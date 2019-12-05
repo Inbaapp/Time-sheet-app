bdacdb68e3787e580097f0b39b5e9b39d58fc46a
Inbaapp <inbaapp@gmail.com>
2019-12-06 01:17:17 +0530

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,22 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import {  environment } from './env/env';
+
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import AngularFireAuthModule from "firebase/auth";
+import "firebase/firestore";
+
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,  AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig), ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/env/env.ts
+++ src/app/env/env.ts
@@ -1,14 +1,8 @@
-import { Component } from '@angular/core';
+ 
 
-@Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
-})
-export class AppComponent  {
-  name = 'Angular';
-}
- firebaseConfig = {
+  export const environment = {
+  production: false,
+  firebaseConfig: {
     apiKey: "AIzaSyDnbwQPgX_pU-D765GdcEfKMAxCid0JUXw",
     authDomain: "timesheetapp-10c5f.firebaseapp.com",
     databaseURL: "https://timesheetapp-10c5f.firebaseio.com",
@@ -16,6 +10,5 @@ export class AppComponent  {
     storageBucket: "timesheetapp-10c5f.appspot.com",
     messagingSenderId: "864308461099",
     appId: "1:864308461099:web:fd7cf3f32676cca076b46e"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  }
+};
